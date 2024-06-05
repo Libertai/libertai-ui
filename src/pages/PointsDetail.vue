b6dd977d52ff2332409bee1685cc008608e8cefb
aliel <ali.elb@gmail.com>
2024-06-05 19:50:26 +0200

    fix mobile page rendering fix scroll over header

--- src/pages/PointsDetail.vue
+++ src/pages/PointsDetail.vue
@@ -1,57 +1,61 @@
 <template>
-  <q-page class="flex flex-center q-pa-xl font-mulish text-purple-700">
-    <div class="row q-pa-xl q-col-gutter-md q-py-xl">
-      <q-card class="col-12 text-center" flat>
-        <q-card-section>
-          <p class="q-py-md">
-            <span class="bg-purple-50 q-py-sm q-px-xl rounded text-bold">{{ address }}</span>
-          </p>
-          <div class="text-h4 text-bold q-pb-sm text-left">Your Libertai Points</div>
-          <p class="text-left">
-            Run aleph.im network nodes, stake ALEPH to continue earning Libertai points. New ways will be available
-            soon!
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="bg-purple-50 col-12 q-pa-none" flat>
-        <q-card-section class="q-pt-none q-pa-xl row">
-          <div class="text-h6 text-bold col-8 text-left">
-            <q-avatar class="q-mr-sm">
-              <img src="icons/svg/libertai.svg" />
-            </q-avatar>
-            Total Points Received
-          </div>
+  <q-scroll-area style="height: calc(100vh - 50px)">
+    <q-page class="flex flex-center text-purple-700 q-mx-md">
+      <div class="row q-pa-xl q-col-gutter-md">
+        <q-card class="col-12 text-center" flat>
+          <q-card-section>
+            <p class="q-py-md">
+              <span class="bg-purple-50 q-py-sm q-px-xl rounded text-bold">
+                {{ address.slice(0, 10) }}...{{ address.slice(-10) }}</span
+              >
+            </p>
+            <div class="text-h4 text-bold q-pb-sm text-left">Your Libertai Points</div>
+            <p class="text-left">
+              Run aleph.im network nodes, stake ALEPH to continue earning Libertai points. New ways will be available
+              soon!
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card class="col-12 column" flat>
+          <q-card-section class="bg-purple-50 q-pa-xl row">
+            <div class="text-h6 text-bold col-8 text-left">
+              <q-avatar class="q-mr-sm">
+                <img src="icons/svg/libertai.svg" />
+              </q-avatar>
+              Total Points Received
+            </div>
 
-          <div class="col-4">
-            <span class="text-h4 text-bold">{{ points.getAddressPoints(address).toFixed(2) }}</span>
-          </div>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column q-pl-none" flat>
-        <q-card-section class="bg-purple-50 q-pa-xl">
-          <p class="text-h6 text-bold text-left">Pending Points</p>
-          <p class="q-py-md text-right">
-            <span class="text-h4 text-bold rounded">{{ currentPendingPoints.toFixed(2) }}</span>
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column" flat>
-        <q-card-section class="bg-purple-50 q-pa-xl">
-          <p class="text-h6 text-bold text-left">36 Month estimated Points*</p>
-          <p class="q-py-md text-right">
-            <span class="text-h4 text-bold rounded">{{ ThreeYearsPoints.toFixed(2) }}</span>
-          </p>
-        </q-card-section>
-      </q-card>
-      <p :class="$q.dark.mode ? 'text-white' : ''">You are getting {{ hourlyRate.toFixed(2) }} points per hour.</p>
-      <p class="text-grey text-center col-12">
-        * Estimate only, and under current rules, if your participation stays at the same level. <br />
-        The availability of Libertai Points is subject to change without notice. We may suspend, modify, or terminate
-        the program at our sole discretion and without liability. Your participation does not guarantee that you will
-        receive any specific amount of points or tokens.
-      </p>
-    </div>
-  </q-page>
+            <div class="col-4">
+              <span class="text-h4 text-bold">{{ points.getAddressPoints(address).toFixed(2) }}</span>
+            </div>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : ' col-12 text-center'" flat>
+          <q-card-section class="bg-purple-50 q-pa-xl">
+            <p class="text-h6 text-bold text-left">Pending Points</p>
+            <p class="q-py-md text-right">
+              <span class="text-h4 text-bold rounded">{{ currentPendingPoints.toFixed(2) }}</span>
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : ' col-12 text-center'" flat>
+          <q-card-section class="bg-purple-50 q-pa-xl">
+            <p class="text-h6 text-bold text-left">36 Month estimated Points*</p>
+            <p class="q-py-md text-right">
+              <span class="text-h4 text-bold rounded">{{ ThreeYearsPoints.toFixed(2) }}</span>
+            </p>
+          </q-card-section>
+        </q-card>
+        <p :class="$q.dark.mode ? 'text-white' : ''">You are getting {{ hourlyRate.toFixed(2) }} points per hour.</p>
+        <p class="text-grey text-center col-12">
+          * Estimate only, and under current rules, if your participation stays at the same level. <br />
+          The availability of Libertai Points is subject to change without notice. We may suspend, modify, or terminate
+          the program at our sole discretion and without liability. Your participation does not guarantee that you will
+          receive any specific amount of points or tokens.
+        </p>
+      </div>
+    </q-page>
+  </q-scroll-area>
 </template>
 
 <script setup>
--- src/pages/PointsInfo.vue
+++ src/pages/PointsInfo.vue
@@ -1,110 +1,112 @@
 <template>
-  <q-page class="flex flex-center text-purple-700">
-    <div class="row q-pa-xl q-col-gutter-md">
-      <q-card class="col-12 text-center" flat>
-        <q-card-section>
-          <div class="text-h4 text-bold">Earn Libertai Points</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none">
-          <p>
-            Are you ready to dive into a new world of decentralized computing, where your contributions are rewarded
-            with tangible benefits? Look no further than Libertai Points! These points play an essential role within the
-            exciting and innovative Aleph.im ecosystem.
-          </p>
+  <q-scroll-area style="height: calc(100vh - 50px)">
+    <q-page class="flex flex-center text-purple-700 q-mx-md">
+      <div class="row q-pa-xl q-col-gutter-md">
+        <q-card class="col-12" flat>
+          <q-card-section>
+            <div class="text-h4 text-bold">Earn Libertai Points</div>
+          </q-card-section>
+          <q-card-section>
+            <p>
+              Are you ready to dive into a new world of decentralized computing, where your contributions are rewarded
+              with tangible benefits? Look no further than Libertai Points! These points play an essential role within
+              the exciting and innovative Aleph.im ecosystem.
+            </p>
 
-          <p>How to participate:</p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column" flat>
-        <q-card-section class="bg-purple-50">
-          <div class="text-h6 text-semibold">Stake ALEPH</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none bg-purple-50 col-grow">
-          <p>
-            Staking 10K or more Aleph tokens ($ALEPH) - By showing your commitment to the future of decentralized cloud
-            technology, you earn Libertai Points that reflect your trust in the platform.
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column" flat>
-        <q-card-section class="bg-purple-50">
-          <div class="text-h6 text-semibold">aleph.im Core Channel Node Operator</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none bg-purple-50 col-grow">
-          <p>
-            Running an Aleph.im Core Channel Node - Become a part of the backbone that supports cross-chain interactions
-            and enjoy the perks of being a node operator with Libertai Points as your reward!
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column" flat>
-        <q-card-section class="bg-purple-50">
-          <div class="text-h6 text-semibold">aleph.im Resource Node Operator</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none bg-purple-50 col-grow">
-          <p>
-            Operating an Aleph.im Resource Node - Contribute to data storage, compute capabilities, and decentralized
-            applications by running a resource node and reap the benefits through Libertai Points!
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-6 text-center column" flat>
-        <q-card-section class="bg-purple-50">
-          <div class="text-h6 text-semibold">Connect your wallet and Start earning points today!</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none bg-purple-50 col-grow">
-          <p>
-            So, are you ready to join this revolutionary journey and unlock the power of Libertai Points? Let's get
-            started together and shape the future of decentralized cloud technology!
-          </p>
-        </q-card-section>
-      </q-card>
-      <q-card class="col-12 text-center" flat>
-        <q-card-section class="bg-purple-50">
-          <div class="text-h6">Check your points</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none bg-purple-50">
-          <q-input
-            v-model="address"
-            :rules="addressRules"
-            autofocus
-            bg-color="secondary"
-            bottom-slots
-            class="q-pa-lg"
-            counter
-            input-class="text-light"
-            label="Address"
-            label-color="grey"
-            maxlength="42"
-            rounded
-            standout
-          >
-            <template v-slot:prepend>
-              <q-icon name="wallet" />
-            </template>
+            <p>How to participate:</p>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : 'text-center'" flat>
+          <q-card-section class="bg-purple-50">
+            <div class="text-h6 text-semibold">Stake ALEPH</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none bg-purple-50 col-grow">
+            <p>
+              Staking 10K or more Aleph tokens ($ALEPH) - By showing your commitment to the future of decentralized
+              cloud technology, you earn Libertai Points that reflect your trust in the platform.
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : 'text-center'" flat>
+          <q-card-section class="bg-purple-50">
+            <div class="text-h6 text-semibold">aleph.im Core Channel Node Operator</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none bg-purple-50 col-grow">
+            <p>
+              Running an Aleph.im Core Channel Node - Become a part of the backbone that supports cross-chain
+              interactions and enjoy the perks of being a node operator with Libertai Points as your reward!
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : 'text-center'" flat>
+          <q-card-section class="bg-purple-50">
+            <div class="text-h6 text-semibold">aleph.im Resource Node Operator</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none bg-purple-50 col-grow">
+            <p>
+              Operating an Aleph.im Resource Node - Contribute to data storage, compute capabilities, and decentralized
+              applications by running a resource node and reap the benefits through Libertai Points!
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card :class="$q.screen.gt.sm ? 'col-6 text-center column' : 'text-center'" flat>
+          <q-card-section class="bg-purple-50">
+            <div class="text-h6 text-semibold">Connect your wallet and Start earning points today!</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none bg-purple-50 col-grow">
+            <p>
+              So, are you ready to join this revolutionary journey and unlock the power of Libertai Points? Let's get
+              started together and shape the future of decentralized cloud technology!
+            </p>
+          </q-card-section>
+        </q-card>
+        <q-card class="col-12 text-center" flat>
+          <q-card-section class="bg-purple-50">
+            <div class="text-h6">Check your points</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none bg-purple-50">
+            <q-input
+              v-model="address"
+              :rules="addressRules"
+              autofocus
+              bg-color="secondary"
+              bottom-slots
+              class="q-pa-lg"
+              counter
+              input-class="text-light"
+              label="Address"
+              label-color="grey"
+              maxlength="42"
+              rounded
+              standout
+            >
+              <template v-slot:prepend>
+                <q-icon name="wallet" />
+              </template>
 
-            <template v-slot:hint> Enter an address to check if there are points associated with it.</template>
-          </q-input>
-          <div v-if="addressVerifier(address) === true" class="text-h6 text-bold q-pa-lg">
-            <div v-if="points.getAddressPoints(address) === 0">
-              You have no point so far. You can still earn points by staking ALEPH or running a node!
+              <template v-slot:hint> Enter an address to check if there are points associated with it.</template>
+            </q-input>
+            <div v-if="addressVerifier(address) === true" class="text-h6 text-bold q-pa-lg">
+              <div v-if="points.getAddressPoints(address) === 0">
+                You have no point so far. You can still earn points by staking ALEPH or running a node!
+              </div>
+              <div v-else>
+                You have points!<br /><br />
+                <q-btn
+                  :to="{ name: 'points-detail', params: { address: address } }"
+                  color="white"
+                  label="View details"
+                  no-caps
+                  rounded
+                  text-color="primary"
+                />
+              </div>
             </div>
-            <div v-else>
-              You have points!<br /><br />
-              <q-btn
-                :to="{ name: 'points-detail', params: { address: address } }"
-                color="white"
-                label="View details"
-                no-caps
-                rounded
-                text-color="primary"
-              />
-            </div>
-          </div>
-        </q-card-section>
-      </q-card>
-    </div>
-  </q-page>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-page>
+  </q-scroll-area>
 </template>
 
 <script setup>
