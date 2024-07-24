d91f0e2ffadc916b7cd20f783368cdab4929bd47
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-07-24 23:29:12 +0200

    fix: Message regeneration only from AI

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -68,7 +68,7 @@
           <div class="absolute dyn-container chat-toolbar">
             <!-- Allow regenerating the last message from the AI if fully completed -->
             <q-btn
-              v-if="!isLoadingRef && message_index === chatRef!.messages.length - 1"
+              v-if="!isLoadingRef && message.author === 'ai'"
               dense
               flat
               icon="refresh"
