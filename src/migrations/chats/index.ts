bb69ec43ff7c715e5a70d1670714d3948ab69eaf
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-08-06 14:56:11 +0200

    feat: Remove chat tags

--- src/migrations/chats/index.ts
+++ src/migrations/chats/index.ts
@@ -3,10 +3,12 @@ import { chat_1_add_message_author } from 'src/migrations/chats/chat_1_add_messa
 import { chat_2_add_persona_role_and_ipfs_avatar } from 'src/migrations/chats/chat_2_add_persona_role_and_ipfs_avatar';
 import { chat_3_add_model_id } from 'src/migrations/chats/chat_3_add_model_id';
 import { chat_4_add_knowledge_bases } from 'src/migrations/chats/chat_4_add_knowledge_bases';
+import { chat_5_remove_tags } from 'src/migrations/chats/chat_5_remove_tags';
 
 export const chatsMigrations: ChatMigration[] = [
   chat_1_add_message_author,
   chat_2_add_persona_role_and_ipfs_avatar,
   chat_3_add_model_id,
   chat_4_add_knowledge_bases,
+  chat_5_remove_tags,
 ];
