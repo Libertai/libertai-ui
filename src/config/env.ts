cdad239f0a0ab0a9065a9d4759f27932f9920ddd
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-11-05 02:28:52 +0900

    feat: Agents setup started

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,9 @@ import { base, baseSepolia } from '@wagmi/vue/chains';
 import { z } from 'zod';
 
 const envSchema = z.object({
-  ALEPH_API_URL: z.string().optional(),
-  LTAI_SUBSCRIPTIONS_API_URL: z.string(),
+  ALEPH_API_URL: z.string().url().optional(),
+  LTAI_SUBSCRIPTIONS_API_URL: z.string().url(),
+  LTAI_AGENTS_API_URL: z.string().url(),
   WALLET_CONNECT_PROJECT_ID: z.string(),
   SOLANA_RPC: z.string().url(),
   LTAI_BASE_ADDRESS: z.string().startsWith('0x').optional().default('0xF8B1b47AA748F5C7b5D0e80C726a843913EB573a'),
---
+++ src/pages/Agents.vue
@@ -0,0 +1,27 @@
+<template>
+  <authenticated-page>
+    <section class="max-sm:tw-mx-4 sm:tw-mx-10 tw-my-5">
+      <div class="tw-flex tw-mb-5 tw-items-center tw-space-x-2">
+        <h4 class="text-h4 text-semibold">Agents</h4>
+        <a href="https://docs.libertai.io/chat/documents/knowledge-base.html" target="_blank">
+          <ltai-icon name="help_outline" size="xs" />
+        </a>
+      </div>
+
+      <p>Manage your agents</p>
+
+      <empty-state
+        description="Contact a team member to get access to the private beta"
+        image-alt="No agents"
+        image-link="/assets/empty-states/knowledge-base.png"
+        title="No agents"
+      />
+    </section>
+  </authenticated-page>
+</template>
+
+<script lang="ts" setup>
+import EmptyState from 'components/EmptyState.vue';
+import LtaiIcon from 'components/libertai/LtaiIcon.vue';
+import AuthenticatedPage from 'layouts/AuthenticatedPage.vue';
+</script>
